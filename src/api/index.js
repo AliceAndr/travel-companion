74ea93c8314d372911e672055be7af369fd2c945
Alice <andreevaalisa42@gmail.com>
2021-09-15 20:27:14 +0300

    fix search

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
-import axios from "axios";
+/* eslint-disable consistent-return */
+import axios from 'axios';
 
 export const getPlacesData = async (type, sw, ne) => {
   try {
@@ -10,12 +11,32 @@ export const getPlacesData = async (type, sw, ne) => {
         tr_latitude: ne.lat,
       },
       headers: {
-        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        // 'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        'x-rapidapi-key': '68340a22bfmsh4cc31f75516ebd6p16f8edjsn9934568971bd',
         'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
       },
     });
 
     return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          // 'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-key': '68340a22bfmsh4cc31f75516ebd6p16f8edjsn9934568971bd',
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
   } catch (error) {
     console.log(error);
   }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles.js';
 
@@ -18,14 +16,6 @@ const Header = ({ onPlaceChanged, onLoad }) => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> */}
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-            </div>
-          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
