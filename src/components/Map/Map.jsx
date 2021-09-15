fd3648db39156e347c3dd3e61df8a37801efa4db
Alice <andreevaalisa42@gmail.com>
2021-09-15 17:37:51 +0300

    add api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -26,6 +26,27 @@ const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherDat
         }}
         onChildClick={(child) => setChildClicked(child)}
       >
+        {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
         {weatherData?.list?.length && weatherData.list.map((data, i) => (
           <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
             <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
